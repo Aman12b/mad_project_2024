cd3730c4a95e6ab4a9d2ef01dfe1adc08a801ce9
Xandi <alexander.mosor@gmail.com>
2024-06-25 16:19:07 +0200

    Adjust to newest changes

--- app/src/main/java/com/example/mad_project/classes/FlightClass.kt
+++ app/src/main/java/com/example/mad_project/classes/FlightClass.kt
@@ -1,22 +1,16 @@
 package com.example.mad_project.classes
 
-import com.google.gson.annotations.SerializedName
-
-data class Flight(
-    @SerializedName("airline") val airline: String,
-    @SerializedName("departure_at") val departureAt: String,
-    @SerializedName("return_at") val returnAt: String,
-    @SerializedName("expires_at") val expiresAt: String,
-    @SerializedName("price") val price: Int,
-    @SerializedName("flight_number") val flightNumber: Int
-)
-
-data class Data(
-    @SerializedName("0") val flight: Flight
-)
-
 data class ApiResponse(
-    @SerializedName("data") val data: Map<String, Data>,
-    @SerializedName("currency") val currency: String,
-    @SerializedName("success") val success: Boolean
+    val data: Map<String, Map<String, FlightInfo>>,
+    val currency: String,
+    val success: Boolean
+)
+
+data class FlightInfo(
+    val airline: String,
+    val departure_at: String,
+    val return_at: String,
+    val expires_at: String,
+    val price: Int,
+    val flight_number: Int
 )
--- app/src/main/java/com/example/mad_project/screens/FlightsScreen.kt
+++ app/src/main/java/com/example/mad_project/screens/FlightsScreen.kt
@@ -1,83 +1,53 @@
 package com.example.mad_project.screens
 
-import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.ViewModel.FlightsViewModel
-import com.example.mad_project.classes.Flight
+import com.example.mad_project.FlightList
 import com.example.movieappmad24.components.Bars.SimpleTopAppBar
+import org.json.JSONObject
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FlightsScreen(
     navController: NavController,
-    viewModel: FlightsViewModel
+    viewModel: FlightsViewModel,
+    jsonString: String
 ) {
+    LaunchedEffect(Unit) {
+        val jsonObject = JSONObject(jsonString)
+        val origin = jsonObject.getString("startLocation")
+        val destination = jsonObject.getString("destination")
+        val startDate = jsonObject.getString("startDate")
+        val endDate = jsonObject.getString("endDate")
 
-    val isLoading by remember { viewModel.isLoading }
+        viewModel.fetchFlights(origin, destination, startDate, endDate)
+    }
 
     Scaffold(
         topBar = {
             SimpleTopAppBar(title = "Flights", navController = navController)
         }
     ) { innerPadding ->
-        Log.i("tset", "TESTTTT");
-        Text("TESTTT");
-        if (isLoading) {
-            Text("Loading...", modifier = Modifier.padding(innerPadding))
-        } else {
-            Column {
+        Column {
+            if (viewModel.isLoading.value) {
+                // Display a loading indicator here
+                Text("Loading...")
+            } else {
+                // Display the list of flights
                 FlightList(
                     modifier = Modifier.padding(innerPadding),
-                    flights = viewModel.flights
+                    navController = navController,
+                    viewModel = viewModel
                 )
             }
         }
-
-    }
-}
-
-@Composable
-fun FlightList(
-    modifier: Modifier = Modifier,
-    flights: List<Flight>
-) {
-    LazyColumn(modifier = modifier) {
-        items(flights) { flight ->
-            FlightItem(flight = flight)
-        }
-    }
-}
-
-@Composable
-fun FlightItem(flight: Flight) {
-    Row(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column {
-            Text(text = "Airline: ${flight.airline}")
-            Text(text = "Departure Time: ${flight.departureAt.subSequence(0, 10)}")
-            Text(text = "Return Time: ${flight.returnAt.subSequence(0, 10)}")
-        }
-        Text(text = "Price: ${flight.price} ")
     }
 }
